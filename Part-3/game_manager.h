1f9e9bc3cb40648a309720ec25b7a503c7b6605d
K4mran <kamran2727@gmail.com>
2019-04-25 21:39:41 +0430

    a very primitive version
    add_player() methods of game_manager are defined in header file

---
+++ Part-3/game_manager.h
@@ -0,0 +1,12 @@
+class game_manager()
+{
+public:
+	add_team_A_goalkeeper();
+	add_team_A_defender();
+	add_team_A_striker();
+	add_team_B_goalkeeper();
+	add_team_B_defender();
+	add_team_B_striker();
+private:
+
+}
---
+++ Part-3/main.cpp
@@ -0,0 +1,7 @@
+#include "game_manager.h"
+
+int main()
+{
+	game_manager game = game_manager()
+	
+}
