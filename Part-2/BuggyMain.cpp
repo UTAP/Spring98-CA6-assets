d2174689b7c657c1c8215a7c0156a3109d205b08
moridi <m.moridi.2009@gmail.com>
2019-04-25 20:22:32 +0430

    Add FileSystemInterface and sample input/output for the 2nd part

---
+++ Part-2/BuggyMain.cpp
@@ -0,0 +1,12 @@
+#include "FileSystemInterface.h"
+
+int main()
+{
+    FileSystemInterface interface = FileSystemInterface();
+    interface.view(0);
+
+    interface.add_directory(1, "etc", 0);
+    interface.view(2);    
+
+    return 0;
+}
